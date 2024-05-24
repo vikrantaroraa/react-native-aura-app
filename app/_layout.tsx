e52dc5f43883857f9d4bbdb9da6a16fd90ff58b0
vikrantaroraa <arora.veshu@gmail.com>
2024-05-24 19:46:49 +0530

    feat: used the Slot component to render the default index.tsx page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="index" />
+//     </Stack>
+//   );
+// }
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return <Slot />;
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screennnnnn bla bla bla.</Text>
     </View>
   );
 }
