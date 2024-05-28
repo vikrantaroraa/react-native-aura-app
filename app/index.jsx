ac0ccbcdcefb49b1d7d25955cb8e09ddbe8f4930
vikrantaroraa <arora.veshu@gmail.com>
2024-05-28 17:17:53 +0530

    feat: added Tabs navigation to (tabs) _layout, created the splash screen for the app

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,49 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "@/components/CustomButton";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-pblack">
-        Edit app/index.tsx to edit this screennnnnn.
-      </Text>
-      <Link href={"/sign-in"} style={{ color: "blue" }}>
-        Go to Sign In
-      </Link>
-      <Link href={"/home"} style={{ color: "red" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="h-full w-full items-center justify-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aura</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="absolute w-[136px] h-[15px] -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 text-center mt-7">
+            Where creativity meets innovation, embark on a journey of limitless
+            exploration with Aura
+          </Text>
+
+          <CustomButton
+            title="Continue with email"
+            handlePress={() => router.push("./sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor="#161622" />
+    </SafeAreaView>
   );
 }
